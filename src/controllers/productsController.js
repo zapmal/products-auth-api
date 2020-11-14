40eeeb571b3c767cb7f67cc54830b9b3766f39e9
Zondazx <mzxgd16@gmail.com>
2020-11-14 11:25:16 -0400

    feat: main routes (products) finished

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -40,16 +40,55 @@ const getProducts = async (request, response) => {
   }
 };
 
-const getProduct = (request, response) => {
+const getProduct = async (request, response) => {
+  const id = request.params.id;
 
+  try {
+    const product = await Product.findById(id);
+
+    response.json(product);
+  } catch (error) {
+    response
+      .status(404)
+      .json(error);
+  }
 };
 
-const updateProduct = (request, response) => {
+const updateProduct = async (request, response) => {
+  const id = request.params.id;
 
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, request.body, {
+    new: true
+    });
+
+    response
+      // .status(204)
+      .status(200)
+      .json(updatedProduct);
+  } catch (error) {
+    response
+      .status(500)
+      .json(error);
+  }
 };
 
-const deleteProduct = (request, response) => {
+const deleteProduct = async (request, response) => {
+  const id = request.params.id;
 
+  try {
+    await Product.findByIdAndDelete(id);
+
+    response
+    // .status(204)
+    .json({
+      message: `Product ${id} successfully deleted.`
+    });
+  } catch (error) {
+    response
+      .status(404)
+      .json(error);
+  }
 };
 
 export {
